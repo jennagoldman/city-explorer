aefef6eb946aaf767470f14c06767c236230bf54
Jenna Goldman <jenna.goldman89@gmail.com>
2020-02-20 12:19:49 -0800

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const request = require('superagent');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Jello World!'));
 
